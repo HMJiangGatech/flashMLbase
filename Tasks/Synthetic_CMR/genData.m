e494412be152d1c8f2ecd7975899e670959e4626
HMJiangGatech <jianghm.ustc@gmail.com>
2018-01-06 18:53:21 +0800

    update Synthetic Data generator for CMR

--- Tasks/Synthetic_CMR/genData.m
+++ Tasks/Synthetic_CMR/genData.m
@@ -2,19 +2,23 @@ rng shuffle
 
 n = 200;
 d = 2000;
+nr = 3;
 sigma = 2.0;
 var = eye(d)*0.5 + 0.5;
 mu = randn(1,d)*0;
-beta = zeros(d,1);
-beta(1) = 3;
-beta(2) = -2;
-beta(4) = 1.5;
+beta = zeros(d,nr);
+beta(1,1) = 3;
+beta(2,1) = -2;
+beta(4,1) = 1.5;
+beta(1,2) = 2;
+beta(3,2) = -1;
+beta(1,3) = 3;
+beta(4,3) = -3;
 % beta(randperm(d,d/10*8)) = beta(randperm(d,d/10*8))*0;
 
 
 z = mvnrnd(mu,var, n);
-y = z*beta + sigma*randn(n,1);
-data = [z y];
+y = z*beta + sigma*randn(n,nr);
 csvwrite('synthetic_data.csv',z)
 csvwrite('synthetic_label.csv',y)
 csvwrite('True_Theta.csv',beta')
@@ -28,7 +32,6 @@ csvwrite('True_Theta.csv',beta')
 
 n = 1000;
 z = mvnrnd(mu,var, n);
-y = z*beta + sigma*randn(n,1);
-data = [z y];
+y = z*beta + sigma*randn(n,nr);
 csvwrite('synthetic_data_test.csv',z)
 csvwrite('synthetic_label_test.csv',y)
